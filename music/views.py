44e172af257f1ba42cda226698d310a7c13d2e37
jhd235 <jhd235@outlook.com>
2017-02-14 20:06:41 +0500

    left only one HttpResponse

--- music/views.py
+++ music/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, World")
+	#return HttpResponse("Hello, World")
 	return HttpResponse("This is from PyCharm")
 # Create your views here.
